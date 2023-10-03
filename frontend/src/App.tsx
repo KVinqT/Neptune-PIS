9cd3297e72e91634803d9b27d7d0974ba18c8205
KVinqT <kevinqt4you@gmail.com>
2023-10-03 20:48:18 +0630

    first-commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
-import { Button } from "@/components/ui/button";
+import { InputForm } from "./component/InputForm";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="w-full h-full">
+      <div className="bg-[#F5F7FB] md:w-[700px] md:h-[70vh] rounded-xl absolute left-0 right-0 top-0 bottom-0 m-auto shadow-2xl">
+        <div className="ml-20 mt-10 font-sans font-bold text-2xl text-[#5068E9]">
+          Sign In
+        </div>
+        <div>
+          <InputForm />
+        </div>
+      </div>
     </div>
   );
 }
