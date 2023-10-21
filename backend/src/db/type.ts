6ce702f736ef6cb33eded87743f4e65c9122495d
Jayson <jaysonchenn17@gmail.com>
2023-10-21 11:05:50 +0630

    api doctors

--- backend/src/db/type.ts
+++ backend/src/db/type.ts
@@ -1,8 +1,14 @@
+export interface BookingInterval {
+  tokenId: number;
+  duration: string;
+}
+
 export interface Slots {
   time: string;
   totalSlot: number;
   bookedSlot: number;
   availableSlot: number;
+  bookingInterval: BookingInterval[];
 }
 
 export interface Availability {
